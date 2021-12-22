dce196282f2fc5ff8816eb8e706bb87db079865c
dragan1810 <d.novakovic1810@yahoo.com>
2021-12-22 19:58:04 +0100

    Initial Commit

---
+++ appcenter-build-native-ios/appcenter_build_native_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  appcenter_build_native_iosApp.swift
+//  appcenter-build-native-ios
+//
+//  Created by Dragan Novakovic on 22.12.21..
+//
+
+import SwiftUI
+
+@main
+struct appcenter_build_native_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
